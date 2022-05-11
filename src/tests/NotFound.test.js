16321a6eaa631b36ed09ea29fbd400e661db2847
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-05-11 02:33:08 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto...', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', { name: /Page requested not/i });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem...', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByRole('img', { name: /Pikachu crying because/i });
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
